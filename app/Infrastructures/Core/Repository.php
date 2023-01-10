d8c7ada92260bbeb0f06d1fba92045a01beb7eb5
Yoshi Virgiawan <achmedyoshivirgiawan@gmail.com>
2023-01-10 23:21:38 +0700

    fix incorrect namespace repository

--- app/Infrastructures/Core/Repository.php
+++ app/Infrastructures/Core/Repository.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace App\Infrastructures\Services\Core;
+namespace App\Infrastructures\Core;
 
-use App\Infrastructures\Services\Core\Interfaces\RepositoryInterface;
-use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Model;
+use App\Infrastructures\Core\Interfaces\RepositoryInterface;
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 abstract class Repository implements RepositoryInterface
 {
@@ -32,9 +32,30 @@ abstract class Repository implements RepositoryInterface
         return $this->query->get();
     }
 
-    public function findById($id): Model
+    public function findById($id): ?Model
     {
         return $this->query->findOrFail($id);
+    }
+
+    public function create(array $dataRequest): Model
+    {
+        return $this->query->create($dataRequest);
+    }
+
+    public function updateById($id, array $dataRequest): ?Model
+    {
+        $data = $this->findById($id);
+        $data->update($dataRequest);
+
+        return $data;
+    }
+
+    public function deleteById($id): Model
+    {
+        $data = $this->findById($id);
+        $data->delete();
+
+        return $data;
     }
 
     public function paginator($size = 50, $page = 1): LengthAwarePaginator
---
+++ app/Infrastructures/Services/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Infrastructures\Services;
+
+use App\Infrastructures\Repositories\UserRepository;
+
+class UserService
+{
+    protected $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->userRepository->all();
+    }
+}
